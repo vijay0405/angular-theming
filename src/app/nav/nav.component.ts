d4f49987a68c8934348f10f042523bbf7b031465
vijay0405 <uvijaysimha@gmail.com>
2020-05-27 23:43:50 +0530

    added nav component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  faLightbulb as faSolidLightbulb,
+  faDollarSign,
+  IconDefinition
+} from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb as faRegularLightbulb } from "@fortawesome/free-regular-svg-icons";
+import { ThemeService } from "src/app/theme/theme.service";
+
+@Component({
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
+})
+export class NavComponent implements OnInit {
+  faLightbulb: IconDefinition;
+  faDollarSign = faDollarSign;
+
+  constructor(
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    this.setLightbulb();
+  }
+
+  setLightbulb() {
+    if (this.themeService.isDarkTheme()) {
+      this.faLightbulb = faRegularLightbulb;
+    } else {
+      this.faLightbulb = faSolidLightbulb;
+    }
+  }
+
+  toggleTheme() {
+    if (this.themeService.isDarkTheme()) {
+      this.themeService.setLightTheme();
+    } else {
+      this.themeService.setDarkTheme();
+    }
+
+    this.setLightbulb();
+  }
+}
